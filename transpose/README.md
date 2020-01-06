56e0993e455db30444a20cedb9c46e1f85e4a818
Mike Golubitsky <mike617@gmail.com>
2020-01-05 23:24:31 -0500

    transpose Giant Steps up half step

---
+++ transpose/README.md
@@ -0,0 +1,17 @@
+I was learning Giant Steps and wanted a quick way to transpose the entire tune, using technology.
+
+## To use
+
+Put a tune's changes into a file in `tunes` dir.  
+Changes formatted as _n_ lines of space-separated chords.
+
+```bash
+# e.g.
+docker-compose run --rm transpose giant_steps.txt
+```
+
+## Limitations!
+
+- Currently only transposing up half step.
+- Tested only with Giant Steps. Some chords not implemented.
+- Some chords get transposed into awkward keys (e.g. "A# D#" vs "Bb Eb").
---
+++ transpose/transpose.py
@@ -0,0 +1,59 @@
+import re
+import sys
+
+
+KEYS = 'ABCDEFG'
+
+
+def letter(symbol):
+    return symbol[0]
+
+
+def accidental(symbol):
+    accidental_found = re.search(r'#|b', symbol)
+    return accidental_found.group(0) if accidental_found else ''
+
+
+def quality(symbol):
+    return re.search(r'(\w[#|b]*)(.*)', symbol).group(2)
+
+
+def one_letter_up(letter):
+    return KEYS[(KEYS.find(letter.upper()) + 1) % 7]
+
+
+def transposed(symbol):
+    if accidental(symbol) == 'b':
+        if letter(symbol).upper() in 'BEADG':
+            return f'{letter(symbol)}{quality(symbol)}'
+        else:
+            raise NotImplementedError(symbol)
+    if accidental(symbol) == '#':
+        if letter(symbol).upper() in 'GCFDA':
+            return f'{one_letter_up(letter(symbol))}{quality(symbol)}'
+        else:
+            raise NotImplementedError(symbol)
+
+    if letter(symbol).upper() in 'GCFDA':
+        return f'{letter(symbol)}#{quality(symbol)}'
+    else:
+        return f'{one_letter_up(letter(symbol))}{quality(symbol)}'
+
+
+def transposed_line(line):
+    return [transposed(symbol) for symbol in line]
+
+
+def tune_transposed_up_half_step(lines_of_chords):
+    return [transposed_line(line) for line in lines_of_chords]
+
+
+def lines_of_chords(filename):
+    with open(filename) as file:
+        return [line.split() for line in list(file)]
+
+
+if __name__ == '__main__':
+    lines = lines_of_chords(f'tunes/{sys.argv[1]}')
+    for line in tune_transposed_up_half_step(lines):
+        print(' '.join(line))
