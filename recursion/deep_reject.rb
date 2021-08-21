3f20b74eeda12f4c01575223934d8b84f2a48ad6
Mikhail Golubitsky <mike617@gmail.com>
2021-08-21 15:00:18 -0400

    add deep reject

--- recursion/deep_reject.rb
+++ recursion/deep_reject.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
+# Compare implementations with and without side effects.
+
 def deep_reject(hash, &block)
   # Deeply rejects key/value pairs where the block returns true.
   # Also rejects key/value pairs whose nested contents are empty after rejection.
   hash.reduce({}) do |result, (key, value)|
     case value
-    when Hash
-      nested_result = deep_reject(value, &block)
-      # Also reject keys whose values are empty hashes
-      nested_result.any? ? result.merge(key => nested_result) : result
     when String
       if block.call(key, value)
         # Don't add the key - reject it.
@@ -16,14 +14,41 @@ def deep_reject(hash, &block)
       else
         result.merge(key => value)
       end
+    when Hash
+      nested_result = deep_reject(value, &block)
+      # Also reject keys whose values are empty hashes
+      nested_result.any? ? result.merge(key => nested_result) : result
     else
       raise 'expected deeply nested hash of hashes of strings'
     end
   end
 end
 
-pp deep_reject({}) { |k| k == 'a' } # => {}
-pp deep_reject({ 'a' => '1' }) { |k| k == 'a' } # => {}
+def deep_reject!(hash, &block)
+  hash.each do |key, value|
+    case value
+    when String
+      hash.delete(key) if block.call(key, value)
+    when Hash
+      deep_reject!(value, &block)
+      # Also reject keys whose values are empty hashes
+      hash.delete(key) if value.empty?
+    else
+      raise 'expected deeply nested hash of hashes of strings'
+    end
+  end
+end
 
-hash = { 'a' => '1', 'b' => '2', 'c' => '3', 'd' => { 'a' => '1' } }
-pp deep_reject(hash) { |k| k == 'a' } # => {"b"=>"2", "c"=>"3"}
+def empty_hash
+  {}
+end
+
+def shallow_hash_with_a
+  { 'a' => '1' }
+end
+
+def hash
+  { 'a' => '1', 'b' => '2', 'c' => '3', 'd' => { 'a' => '1' } }
+end
+
+REJECTOR = ->(k, _v) { k == 'a' }
