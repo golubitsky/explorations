bed519dc0ddea0908f8cbc00445c368e5c452a1a
Mikhail Golubitsky <mike617@gmail.com>
2021-09-30 02:22:26 -0400

    find 12 quadritonal arpeggios

--- theory/slominsky_quadritonal_arpeggios.rb
+++ theory/slominsky_quadritonal_arpeggios.rb
@@ -10,93 +10,74 @@ require 'set'
 # Quadritonal Arpeggios are a set of 4 mutually exclusive triads.
 # This means that none of these triads share the same note.
 
-# diminished  0 +3 +6
-# minor       0 +3 +7
-# major       0 +4 +7
-# augmented   0 +4 +8
+# In this program, notes are represented by the integers 0 through 11 inclusive.
+# For output, notes are translated by index:
+NOTE_NAMES = %w[C Db D Eb E F Gb G Ab A Bb B].freeze
 
-# 0 1 2 3 4 5
-# 0   2   4
-#   1   3   5
+ALL_NOTES_IN_OCTAVE = Set.new([*0..11])
 
-# simple case: universe of starting notes without chord inversions
-# possible starting notes
-# dim [0..5] # total: 6
-# min [0..4] # total: 5
-# maj [0..4] # total: 5
-# aug [0..3] # total: 4
-
-# notes are represented by integers [0..11]
-# iterate over triad types
-#   iterate over each available starting position (based on unused notes)
-
+# Intervals are measured in half-steps:
 TRIAD_INTERVALS_BY_QUALITY = {
-  dim: [0, 3, 6],
-  min: [0, 3, 7],
-  maj: [0, 4, 7],
-  aug: [0, 4, 8]
-}
-
-POSSIBLE_STARTING_NOTES_BY_QUALITY = {
-  dim: [*0..5],
-  min: [*0..4],
-  maj: [*0..4],
-  aug: [*0..3]
-}
-
-ALL_NOTES_IN_OCTAVE = Set.new([0..11])
+  diminished: [0, 3, 6],
+  minor: [0, 3, 7],
+  major: [0, 4, 7],
+  augmented: [0, 4, 8]
+}.freeze
 
 QUALITIES = TRIAD_INTERVALS_BY_QUALITY.keys.freeze
 
-def triad(starting_note, triad_intervals)
-  triad_intervals.map { |interval| starting_note + interval }
+def triad(starting_note, quality)
+  TRIAD_INTERVALS_BY_QUALITY[quality]
+    .map { |interval| starting_note + interval }
+    .map { |note| note % 12 }
+    .sort
 end
 
-def eligible_triad?(triad, quadritonal_arpeggio)
-  quadritonal_arpeggio.flatten.each do |note_in_arpeggio|
-    return false if triad.include?(note_in_arpeggio)
-  end
-
-  true
+def all_possible_triads(quality)
+  ALL_NOTES_IN_OCTAVE
+    .map { |starting_note| triad(starting_note, quality) }
+    .uniq # two inverted augmented chords can contain the same notes
 end
 
-def debug(level, string)
-  puts "level #{level} #{string}"
+def all_possible_triads_by_quality
+  QUALITIES.to_h { |quality| [quality, all_possible_triads(quality)] }
 end
 
-# naive means: do not consider any chord inversions
-def quadritonal_arpeggio_naively(qualities = QUALITIES, quadritonal_arpeggio = [], level = 0)
-  debug(level, "qualities: #{qualities}")
-  return quadritonal_arpeggio if qualities.empty?
+def quadritonal_arpeggio?(arpeggio)
+  arpeggio.flatten.uniq == 12
+end
 
-  qualities.each do |quality|
-    POSSIBLE_STARTING_NOTES_BY_QUALITY[quality].each do |starting_note|
-      possible_triad = triad(starting_note, TRIAD_INTERVALS_BY_QUALITY[quality])
-      debug(level, "trying #{possible_triad}")
+def all_notes_unique?(*triads)
+  triads.flatten.uniq.count == triads.count * 3
+end
 
-      if eligible_triad?(possible_triad, quadritonal_arpeggio)
-        quadritonal_arpeggio << possible_triad
+def quadritonal_arpeggios
+  possible_triads = all_possible_triads_by_quality
+  quadritonal_arpeggios = []
 
-        remaining_qualities = qualities.reject { |q| q == quality }
-        next_level_arpeggio = quadritonal_arpeggio_naively(remaining_qualities, quadritonal_arpeggio, level + 1)
+  possible_triads[:diminished].each do |dim_triad|
+    possible_triads[:minor].each do |min_triad|
+      possible_triads[:major].each do |maj_triad|
+        possible_triads[:augmented].each do |aug_triad|
+          next unless all_notes_unique?(dim_triad, min_triad, maj_triad, aug_triad)
 
-        debug(level, ">>>next_level_arpeggio #{next_level_arpeggio}")
-      else
-        debug(level, "ineligible #{possible_triad}")
-        next
+          quadritonal_arpeggios << [dim_triad, min_triad, maj_triad, aug_triad]
+        end
       end
     end
   end
 
-  quadritonal_arpeggio
+  quadritonal_arpeggios
 end
 
-# p QUALITIES
-quadritonal_arpeggio_naively
-p 'hi'
+def as_notes(arpeggio)
+  arpeggio.map do |triad|
+    triad.map { |note| NOTE_NAMES[note] }.join(' ')
+  end
+end
 
-# TODO: above is a failed attempt.
-# 1. inversions -- obviously need to consider (i.e. fix the naive aspect)
-# 2. try it from another angle: build all the combinations of 4 random 3-note combos in 12 notes,
-# -  and then select those that are entirely composed of one of the _actual_ triads
-# - ^^^ this seems like the way to go
+quadritonal_arpeggios.each do |arpeggio|
+  p as_notes(arpeggio)
+end
+
+puts "Found #{quadritonal_arpeggios.count} quadritonal arpeggios, listed above."
