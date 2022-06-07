dfdd5e025b6efddf17a4bf5da6818abeb8e042e2
Mikhail Golubitsky <mike617@gmail.com>
2022-06-06 23:16:00 -0400

    add A Self-Assessment Test

--- art_of_software_testing/lib/triangle.rb
+++ art_of_software_testing/lib/triangle.rb
@@ -1,5 +1,35 @@
 # frozen_string_literal: true
 
-def hello
-  'test'
+def triangle(card)
+  raise 'invalid input' unless valid_card?(card)
+
+  {
+    1 => 'equilateral',
+    2 => 'isosceles',
+    3 => 'scalene'
+  }[card.uniq.count]
+end
+
+def valid_card?(card)
+  exactly_three_sides?(card) &&
+    only_integers?(card) &&
+    all_sides_positive?(card) &&
+    passes_triangle_inequality_theorem?(card)
+end
+
+def exactly_three_sides?(card)
+  card.count == 3
+end
+
+def only_integers?(card)
+  card.all? { |side| side.is_a?(Integer) }
+end
+
+def all_sides_positive?(card)
+  card.all?(&:positive?)
+end
+
+def passes_triangle_inequality_theorem?(card)
+  a, b, c = card
+  a + b > c && a + c > b && b + c > a
 end
--- art_of_software_testing/spec/triangle_spec.rb
+++ art_of_software_testing/spec/triangle_spec.rb
@@ -1,9 +1,54 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe 'A Self-Assessment Test' do
-  describe 'hello' do
-    it 'should import from lib' do
-      expect(hello).to eq('test')
+  describe 'identification of triangles' do
+    [
+      [[1, 1, 1], 'equilateral'],
+      # any order isosceles
+      [[2, 2, 1], 'isosceles'],
+      [[2, 1, 2], 'isosceles'],
+      [[1, 2, 2], 'isosceles'],
+      # any order scalene
+      [[3, 4, 5], 'scalene'],
+      [[3, 5, 4], 'scalene'],
+      [[4, 3, 5], 'scalene'],
+      [[4, 5, 3], 'scalene'],
+      [[5, 4, 3], 'scalene'],
+      [[5, 3, 4], 'scalene']
+    ].each do |(card, triangle_type)|
+      it "identifies #{card.join(', ')} as #{triangle_type}" do
+        expect(triangle(card)).to eq(triangle_type)
+      end
+    end
+
+    describe 'detection of non-triangles' do
+      [
+        # negative side
+        [-1, 1, 1],
+        [1, -1, 1],
+        [1, 1, -1],
+        # zero side - maybe not needed? Probably impossible to break inequality
+        # theorem with a zero side
+        [0, 2, 2],
+        [2, 0, 2],
+        [2, 2, 0],
+        # breaks triangle inequality theorem
+        [2, 1, 1],
+        [1, 2, 1],
+        [1, 1, 2],
+        # only accepts integers (even if triangle is otherwise valid)
+        %w[1 1 1],
+        # too many or too few values
+        [],
+        [1, 1],
+        [1, 1, 1, 1]
+      ].each do |card_with_non_triangle|
+        it "rejects invalid input #{card_with_non_triangle.inspect}" do
+          expect { triangle(card_with_non_triangle) }.to raise_error('invalid input')
+        end
+      end
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
