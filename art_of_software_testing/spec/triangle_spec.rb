f43311d81e83ff967c1d04ca34f025e8b03abfe5
Mikhail Golubitsky <mike617@gmail.com>
2022-06-08 16:45:27 -0400

    un-nest tests about the detection of non-triangles

--- art_of_software_testing/spec/triangle_spec.rb
+++ art_of_software_testing/spec/triangle_spec.rb
@@ -21,37 +21,37 @@ RSpec.describe 'A Self-Assessment Test' do
         expect(triangle(card)).to eq(triangle_type)
       end
     end
+  end
 
-    describe 'detection of non-triangles' do
-      [
-        # negative side
-        [-1, 1, 1],
-        [1, -1, 1],
-        [1, 1, -1],
-        # zero side
-        [0, 2, 2],
-        [2, 0, 2],
-        [2, 2, 0],
-        # all zero sides
-        [0, 0, 0],
-        # breaks triangle inequality theorem; sum of 2 sides equals 3rd
-        [2, 1, 1],
-        [1, 2, 1],
-        [1, 1, 2],
-        # breaks triangle inequality theorem; sum of 2 sides less than 3rd
-        [100, 7, 11],
-        [7, 100, 11],
-        [7, 11, 100],
-        # only accepts integers (even if triangle is otherwise valid)
-        %w[1 1 1],
-        # too many or too few values
-        [],
-        [1, 1],
-        [1, 1, 1, 1]
-      ].each do |card_with_non_triangle|
-        it "rejects invalid input #{card_with_non_triangle.inspect}" do
-          expect { triangle(card_with_non_triangle) }.to raise_error('invalid input')
-        end
+  describe 'detection of non-triangles' do
+    [
+      # negative side
+      [-1, 1, 1],
+      [1, -1, 1],
+      [1, 1, -1],
+      # zero side
+      [0, 2, 2],
+      [2, 0, 2],
+      [2, 2, 0],
+      # all zero sides
+      [0, 0, 0],
+      # breaks triangle inequality theorem; sum of 2 sides equals 3rd
+      [2, 1, 1],
+      [1, 2, 1],
+      [1, 1, 2],
+      # breaks triangle inequality theorem; sum of 2 sides less than 3rd
+      [100, 7, 11],
+      [7, 100, 11],
+      [7, 11, 100],
+      # only accepts integers (even if triangle is otherwise valid)
+      %w[1 1 1],
+      # too many or too few values
+      [],
+      [1, 1],
+      [1, 1, 1, 1]
+    ].each do |card_with_non_triangle|
+      it "rejects invalid input #{card_with_non_triangle.inspect}" do
+        expect { triangle(card_with_non_triangle) }.to raise_error('invalid input')
       end
     end
   end
