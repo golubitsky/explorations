e2c29d10ec4936f0e8a095e1dbdee7cb500e790c
Mikhail Golubitsky <mike617@gmail.com>
2023-03-25 16:17:27 -0400

    finish the rest of the steps by 'cheating'

--- coding_challenges_crickett/02_parser/spec/parser_spec.rb
+++ coding_challenges_crickett/02_parser/spec/parser_spec.rb
@@ -6,7 +6,7 @@ require 'spec_helper'
 
 RSpec.describe 'parsing JSON' do
   subject(:result) do
-    _stdin, stdout, stderr, wait_thr = Open3.popen3(command)
+    _stdin, stdout, stderr, wait_thr = Open3.popen3("ruby parser.rb #{file_path}")
 
     {
       stdout: stdout.read,
@@ -15,12 +15,19 @@ RSpec.describe 'parsing JSON' do
     }
   end
 
-  describe 'step 1' do
+  [
+    'spec/data/step1/valid.json',
+    'spec/data/step2/valid.json',
+    'spec/data/step2/valid2.json',
+    'spec/data/step3/valid.json',
+    'spec/data/step4/valid.json',
+    'spec/data/step4/valid2.json'
+  ].each do |path_to_valid_file|
     context 'when valid JSON' do
-      let(:command) { 'ruby parser.rb spec/data/step1/valid.json' }
+      let(:file_path) { path_to_valid_file }
 
       it 'prints to stdout that input is valid JSON' do
-        expect(result[:stdout]).to eq("spec/data/step1/valid.json is valid JSON\n")
+        expect(result[:stdout]).to eq("#{path_to_valid_file} is valid JSON\n")
       end
 
       it 'does not print to stderr' do
@@ -31,12 +38,20 @@ RSpec.describe 'parsing JSON' do
         expect(result[:exit_code]).to eq(0)
       end
     end
+  end
 
+  [
+    'spec/data/step1/invalid.json',
+    'spec/data/step2/invalid.json',
+    'spec/data/step2/invalid2.json',
+    'spec/data/step3/invalid.json',
+    'spec/data/step4/invalid.json'
+  ].each do |path_to_invalid_file|
     context 'when invalid JSON' do
-      let(:command) { 'ruby parser.rb spec/data/step1/invalid.json' }
+      let(:file_path) { path_to_invalid_file }
 
       it 'prints to stdout that input is invalid JSON' do
-        expect(result[:stdout]).to eq("spec/data/step1/invalid.json is invalid JSON\n")
+        expect(result[:stdout]).to eq("#{file_path} is invalid JSON\n")
       end
 
       it 'does not print to stderr' do
