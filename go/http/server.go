92260c91abec901c9f1a4efbc22169781f7366a8
Mikhail Golubitsky <mike617@gmail.com>
2024-03-27 21:45:59 -0400

    refactor and add a Not Found page

--- go/http/server.go
+++ go/http/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,12 +15,28 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1>")
+}
 
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(404)
+	fmt.Fprint(w, "<h1>Page not found</h1>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	log := strings.Join([]string{"Processing", r.Method, r.URL.Path}, " ")
+	fmt.Println(log)
+
+	if r.URL.Path == "/" {
+		homeHandler(w, r)
+	} else if r.URL.Path == "/contact" {
+		contactHandler(w, r)
+	} else {
+		notFoundHandler(w, r)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting server on :3000")
 	http.ListenAndServe(":3000", nil)
 }
