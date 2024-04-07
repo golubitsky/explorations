58cd35cc424f9c4919c48c91c3a2f6cea509daee
Mikhail Golubitsky <mike617@gmail.com>
2024-04-07 17:53:36 -0400

    refactor logging with custom middleware

--- go/http/server.go
+++ go/http/server.go
@@ -8,34 +8,35 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func logRequest(r *http.Request) {
-	log := strings.Join([]string{"Processing", r.Method, r.URL.Path}, " ")
-	fmt.Println(log)
-}
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	logRequest(r)
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "{\"hello\":\"world\"}")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	logRequest(r)
-
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1>")
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	logRequest(r)
-
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Faq Page</h1>")
 }
 
+func requestLoggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log := strings.Join([]string{"Processing", r.Method, r.URL.Path}, " ")
+		fmt.Println(log)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	r := chi.NewRouter()
+	r.Use(requestLoggerMiddleware)
+
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
@@ -45,12 +46,10 @@ func main() {
 			fmt.Fprint(w, "fetching user ", userId)
 		})
 	})
-
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		logRequest(r)
-
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
+
 	fmt.Println("Starting server on :3000")
 	http.ListenAndServe(":3000", r)
 }
