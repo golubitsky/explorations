f823156d42f6834b567b52ac627ad8b4e8592c96
Mikhail Golubitsky <mike617@gmail.com>
2024-04-07 17:39:10 -0400

    refactor to use chi router

--- go/http/server.go
+++ go/http/server.go
@@ -4,44 +4,46 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+
+	"github.com/go-chi/chi/v5"
 )
 
+func logRequest(r *http.Request) {
+	log := strings.Join([]string{"Processing", r.Method, r.URL.Path}, " ")
+	fmt.Println(log)
+}
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	logRequest(r)
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "{\"hello\":\"world\"}")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
+	logRequest(r)
+
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1>")
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
+	logRequest(r)
+
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Faq Page</h1>")
 }
 
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log := strings.Join([]string{"Processing", r.Method, r.URL.Path}, " ")
-	fmt.Println(log)
-
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}
-
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		logRequest(r)
+
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Starting server on :3000")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
