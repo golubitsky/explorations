e04d133e579ba25fef87b8d1512010c1ed63eada
Mikhail Golubitsky <mike617@gmail.com>
2024-08-26 16:52:54 -0400

    solve day 6 part 2

--- dsa/advent-of-code/2023/06.py
+++ dsa/advent-of-code/2023/06.py
@@ -1,6 +1,7 @@
 import re
 import math
 
+
 def part_one(data):
     data = [[int(x) for x in re.findall(r"\d+", line)] for line in data]
     time, distance = data
@@ -20,7 +21,22 @@ def part_one(data):
     return math.prod(ways_to_win)
 
 
+def part_two(data):
+    data = [int("".join(re.findall(r"\d+", line))) for line in data]
+    time, best_distance = data
+
+    ways_to_win = 0
+
+    for time_held_down in range(1, time + 1):
+        time_remaining = time - time_held_down
+        distance = time_held_down * time_remaining
+        if distance > best_distance:
+            ways_to_win += 1
+
+    return ways_to_win
+
+
 if __name__ == "__main__":
     with open("06_input.txt", "r") as file:
         data = file.readlines()
-    print(part_one(data))
+    print(part_two(data))
