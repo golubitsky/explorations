575c8ed8dbef50b05a3db656a1fe3c235ce69610
Mikhail Golubitsky <mike617@gmail.com>
2024-08-27 18:27:19 -0400

    day 11: optimize for part 2

--- dsa/advent-of-code/2023/day_11.py
+++ dsa/advent-of-code/2023/day_11.py
@@ -14,29 +14,6 @@ def contains_only_empty_space(row):
     return set(row) == {EMPTY_SPACE}
 
 
-def with_expanded_rows(universe):
-    expanded_rows = []
-
-    for row in universe:
-        expanded_rows.append(row)
-
-        if contains_only_empty_space(row):
-            expanded_rows.append(row)
-
-    return expanded_rows
-
-
-def print_universe(universe):
-    for row in universe:
-        print("".join(row))
-
-
-def with_expanded_columns(universe):
-    universe = transposed(universe)
-    universe = with_expanded_rows(universe)
-    return transposed(universe)
-
-
 def galaxy_coordinates(universe):
     coordinates = []
     for y, row in enumerate(universe):
@@ -47,14 +24,36 @@ def galaxy_coordinates(universe):
     return coordinates
 
 
-def length_of_shortest_path(galaxy, other_galaxy):
-    return sum(
-        [
-            max(galaxy[dimension], other_galaxy[dimension])
-            - min(galaxy[dimension], other_galaxy[dimension])
-            for dimension in ["x", "y"]
-        ]
-    )
+def length_of_shortest_path(
+    galaxy, other_galaxy, empty_row_indexes, empty_col_indexes, expansion_multiplier
+):
+    empty_indexes = {"x": empty_col_indexes, "y": empty_row_indexes}
+    extra_distance_for_expansion_multiplier = (
+        expansion_multiplier - 1
+    )  # account for the row being expanded
+
+    def index_between_galaxies(index, dimension):
+        return index < max(galaxy[dimension], other_galaxy[dimension]) and index > min(
+            galaxy[dimension], other_galaxy[dimension]
+        )
+
+    def dimension_distance_for_expansion(d):
+        return sum(
+            [
+                extra_distance_for_expansion_multiplier
+                for index in empty_indexes[d]
+                if index_between_galaxies(index, d)
+            ]
+        )
+
+    def dimension_distance(d):
+        return (
+            max(galaxy[d], other_galaxy[d])
+            - min(galaxy[d], other_galaxy[d])
+            + dimension_distance_for_expansion(d)
+        )
+
+    return sum([dimension_distance(dimension) for dimension in ["x", "y"]])
 
 
 def pairs_of_galaxies(galaxies):
@@ -69,25 +68,51 @@ def pairs_of_galaxies(galaxies):
     return pairs
 
 
-def lengths_of_shortest_path_between_every_pair_of_galaxies(galaxies):
+def lengths_of_shortest_path_between_every_pair_of_galaxies(
+    galaxies, empty_row_indexes, empty_column_indexes, expansion_multiplier
+):
     return [
-        length_of_shortest_path(galaxy, other_galaxy)
+        length_of_shortest_path(
+            galaxy,
+            other_galaxy,
+            empty_row_indexes,
+            empty_column_indexes,
+            expansion_multiplier,
+        )
         for galaxy, other_galaxy in pairs_of_galaxies(galaxies)
     ]
 
 
-def part_one(data):
+def empty_row_indexes(universe):
+    return [
+        index for index, row in enumerate(universe) if contains_only_empty_space(row)
+    ]
+
+
+def empty_column_indexes(universe):
+    return empty_row_indexes(transposed(universe))
+
+
+def solution(data, expansion_multiplier):
     universe = parsed_as_matrix(data)
-
-    universe = with_expanded_rows(universe)
-    universe = with_expanded_columns(universe)
-
     galaxies = galaxy_coordinates(universe)
+    lengths = lengths_of_shortest_path_between_every_pair_of_galaxies(
+        galaxies,
+        empty_row_indexes(universe),
+        empty_column_indexes(universe),
+        expansion_multiplier,
+    )
 
-    return sum(lengths_of_shortest_path_between_every_pair_of_galaxies(galaxies))
+    return sum(lengths)
+
+
+def print_universe(universe):
+    for row in universe:
+        print("".join(row))
 
 
 if __name__ == "__main__":
     with open("day_11_input.txt", "r") as file:
         data = file.readlines()
-    print(part_one(data))
+    print(solution(data, expansion_multiplier=2))
+    print(solution(data, expansion_multiplier=1000000))
