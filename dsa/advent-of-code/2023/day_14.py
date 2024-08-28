c276f4a41492efd3759c244eb89f7b480c7ba0bc
Mikhail Golubitsky <mike617@gmail.com>
2024-08-27 21:59:51 -0400

    day 14: add ability to run cycles for part 2

--- dsa/advent-of-code/2023/day_14.py
+++ dsa/advent-of-code/2023/day_14.py
@@ -13,15 +13,21 @@ def parsed_as_matrix(data):
 
 
 def tilt_until_rocks_stop(matrix, direction):
-    def out_of_bounds(y):
+    def out_of_bounds(y, x):
         return y < 0 or x < 0 or y == len(matrix) or x == len(matrix[0])
 
     def is_boundary(y, x):
-        return matrix[y][x] in {ROUND_ROCK, CUBE_ROCK} or out_of_bounds(y)
+        return out_of_bounds(y, x) or matrix[y][x] in {ROUND_ROCK, CUBE_ROCK}
 
     def next_pos(y, x, direction):
         if direction == NORTH:
             return (y - 1, x)
+        elif direction == WEST:
+            return (y, x - 1)
+        elif direction == SOUTH:
+            return (y + 1, x)
+        elif direction == EAST:
+            return (y, x + 1)
 
     def rolled_position(y, x, direction):
         while True:
@@ -32,14 +38,22 @@ def tilt_until_rocks_stop(matrix, direction):
 
         return (y, x)
 
-    for y in range(len(matrix)):
-        for x in range(len(matrix[0])):
-            if matrix[y][x] != ROUND_ROCK:
-                continue
+    def roll_round_rock(y, x):
+        if matrix[y][x] != ROUND_ROCK:
+            return
 
-            y_rolled, x_rolled = rolled_position(y, x, direction)
-            matrix[y][x] = EMPTY
-            matrix[y_rolled][x_rolled] = ROUND_ROCK
+        y_rolled, x_rolled = rolled_position(y, x, direction)
+        matrix[y][x] = EMPTY
+        matrix[y_rolled][x_rolled] = ROUND_ROCK
+
+    if direction in [NORTH, WEST]:
+        for y in range(len(matrix)):
+            for x in range(len(matrix[0])):
+                roll_round_rock(y, x)
+    else:
+        for y in range(len(matrix) - 1, -1, -1):
+            for x in range(len(matrix[0]) - 1, -1, -1):
+                roll_round_rock(y, x)
 
 
 def total_load(matrix):
@@ -61,12 +75,29 @@ def part_one(data):
     print(total_load(matrix))
 
 
+def run_tile_cycle(matrix, directions):
+    for direction in directions:
+        tilt_until_rocks_stop(matrix, direction)
+
+
+def part_two(data):
+    matrix = parsed_as_matrix(data)
+    directions = [NORTH, WEST, SOUTH, EAST]
+    n_cycles = 10000
+
+    for _ in range(n_cycles):
+        run_tile_cycle(matrix, directions)
+    print_matrix(matrix)
+    print(total_load(matrix))
+
+
 def print_matrix(matrix):
+    print("----------")
     for row in matrix:
         print("".join(row))
 
 
 if __name__ == "__main__":
-    with open("day_14_input.txt", "r") as file:
+    with open("day_14_sample.txt", "r") as file:
         data = file.readlines()
-    part_one(data)
+    part_two(data)
