2a693bdc2276bbd0c3593ba3eae2660ee471d60a
Mikhail Golubitsky <mike617@gmail.com>
2024-08-27 21:39:54 -0400

    day 14: refactor to enable part 2

--- dsa/advent-of-code/2023/day_14.py
+++ dsa/advent-of-code/2023/day_14.py
@@ -2,21 +2,34 @@ ROUND_ROCK = "O"
 CUBE_ROCK = "#"
 EMPTY = "."
 
+NORTH = "N"
+WEST = "W"
+SOUTH = "S"
+EAST = "E"
+
 
 def parsed_as_matrix(data):
     return [list(row.strip()) for row in data]
 
 
-def tilt_north_until_stopped(matrix):
+def tilt_until_rocks_stop(matrix, direction):
     def out_of_bounds(y):
-        return y < 0
+        return y < 0 or x < 0 or y == len(matrix) or x == len(matrix[0])
 
     def is_boundary(y, x):
         return matrix[y][x] in {ROUND_ROCK, CUBE_ROCK} or out_of_bounds(y)
 
-    def rolled_position(y, x):
-        while not is_boundary(y - 1, x):
-            y -= 1
+    def next_pos(y, x, direction):
+        if direction == NORTH:
+            return (y - 1, x)
+
+    def rolled_position(y, x, direction):
+        while True:
+            pos = next_pos(y, x, direction)
+            if is_boundary(*pos):
+                break
+            y, x = pos
+
         return (y, x)
 
     for y in range(len(matrix)):
@@ -24,7 +37,7 @@ def tilt_north_until_stopped(matrix):
             if matrix[y][x] != ROUND_ROCK:
                 continue
 
-            y_rolled, x_rolled = rolled_position(y, x)
+            y_rolled, x_rolled = rolled_position(y, x, direction)
             matrix[y][x] = EMPTY
             matrix[y_rolled][x_rolled] = ROUND_ROCK
 
@@ -44,7 +57,7 @@ def total_load(matrix):
 
 def part_one(data):
     matrix = parsed_as_matrix(data)
-    tilt_north_until_stopped(matrix)
+    tilt_until_rocks_stop(matrix, direction=NORTH)
     print(total_load(matrix))
 
 
