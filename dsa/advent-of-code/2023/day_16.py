19a454ee84690ce6c73d2ee4f14e253ba89237bb
Mikhail Golubitsky <mike617@gmail.com>
2024-08-28 02:20:11 -0400

    day 16: solve for part 2

--- dsa/advent-of-code/2023/day_16.py
+++ dsa/advent-of-code/2023/day_16.py
@@ -60,7 +60,21 @@ def print_matrix(matrix, beams):
         print()
 
 
-def part_one(data):
+def possible_starting_beams(matrix):
+    beams = []
+
+    for x in range(len(matrix[0])):
+        beams.append(Beam(y=0, x=x, heading=DOWN))
+        beams.append(Beam(y=len(matrix) - 1, x=x, heading=UP))
+
+    for y in range(len(matrix)):
+        beams.append(Beam(y=y, x=0, heading=RIGHT))
+        beams.append(Beam(y=y, x=len(matrix[0]) - 1, heading=LEFT))
+
+    return beams
+
+
+def solution(data):
     def is_at_wall(beam):
         y, x = beam.y, beam.x
         return y < 0 or y == len(matrix) or x < 0 or x == len(matrix[0])
@@ -107,25 +121,37 @@ def part_one(data):
             if is_at_wall(beam):
                 continue  # i.e., disappear
 
-            energized.add((beam.y, beam.x))
+            energized_tiles.add((beam.y, beam.x))
 
             for beam in with_split(beam):
+                if beam in beam_cache:
+                    continue  # kill the beam!!
+
+                beam_cache.add(beam)
                 new_beams.append(beam)
 
         return new_beams
 
     matrix = parsed_as_matrix(data)
-    energized = set()
-    beams = [Beam(y=0, x=0, heading=RIGHT)]
-    energized.add((beams[0].y, beams[0].x))
-    while True:
-        beams = moved_beams(beams)
-        # sleep(0.2)
-        # print_matrix(matrix, beams)
-        print(len(energized))
+    max_energy = 0
+    for starting_beam in possible_starting_beams(matrix):
+        energized_tiles = set()
+        beam_cache = set()
+
+        beams = [starting_beam]
+        energized_tiles.add((starting_beam.y, starting_beam.x))
+        beam_cache.add(starting_beam)
+
+        while beams:
+            beams = moved_beams(beams)
+            # sleep(2.2)
+            # print_matrix(matrix, beams)
+        if len(energized_tiles) > max_energy:
+            max_energy = len(energized_tiles)
+    print(max_energy)
 
 
 if __name__ == "__main__":
     with open("day_16_input.txt", "r") as file:
         data = file.readlines()
-    part_one(data)
+    solution(data)
