9c04b2d75b17575666b6927886bcf091661b8802
Mikhail Golubitsky <mike617@gmail.com>
2024-08-29 05:46:11 -0400

    day 19: WIP part 2

--- dsa/advent-of-code/2023/day_19.py
+++ dsa/advent-of-code/2023/day_19.py
@@ -1,5 +1,7 @@
 import re
 
+from copy import deepcopy
+
 
 def parsed_workflows(workflows_raw):
     workflows = {}
@@ -84,6 +86,117 @@ def run_sorting_process(parts, workflows):
     return results
 
 
+def default_filter():
+    return {
+        "x": {"min": 1, "max": 4000},
+        "m": {"min": 1, "max": 4000},
+        "a": {"min": 1, "max": 4000},
+        "s": {"min": 1, "max": 4000},
+    }
+
+
+def accept_rule(reject_rule):
+    new_rule = deepcopy(reject_rule)
+    new_rule["destination"]
+    if reject_rule["operator"] == "<":
+        new_rule["operator"] = ">"
+        new_rule["value"] = reject_rule["value"] - 1
+    else:
+        new_rule["operator"] = "<"
+        new_rule["value"] = reject_rule["value"] + 1
+
+    return new_rule
+
+
+def is_split(rule):
+    return "operator" in rule
+
+
+def matching_filter(filter, rule, workflows=[]):
+    new_filter = deepcopy(filter)
+    category = rule["category"]
+
+    if rule["destination"] == "A" or rule["destination"] in workflows:
+        if rule["operator"] == "<":
+            new_filter[category]["max"] = min(
+                new_filter[category]["max"], rule["value"] - 1
+            )
+        elif rule["operator"] == ">":
+            new_filter[category]["min"] = max(
+                new_filter[category]["min"], rule["value"] + 1
+            )
+
+    return new_filter
+
+
+def non_matching_filter(filter, rule, workflows=[]):
+    new_filter = deepcopy(filter)
+
+    category = rule["category"]
+
+    if rule["destination"] == "A" or rule["destination"] in workflows:
+        if rule["operator"] == "<":
+            new_filter[category]["min"] = max(
+                new_filter[category]["min"], rule["value"]
+            )
+        elif rule["operator"] == ">":
+            new_filter[category]["max"] = min(
+                new_filter[category]["max"], rule["value"]
+            )
+    if rule["destination"] == "R":
+        if rule["operator"] == "<":
+            new_filter[category]["min"] = max(
+                new_filter[category]["min"], rule["value"]
+            )
+        elif rule["operator"] == ">":
+            new_filter[category]["max"] = min(
+                new_filter[category]["max"], rule["value"]
+            )
+        pass
+
+    return new_filter
+
+
+def find_all_filters(workflows):
+    all_filters = []
+
+    def search(rules, filter):
+        if not rules:
+            return
+
+        rule, *remaining_rules = rules
+
+        if is_split(rule):
+            if rule["destination"] == "A":
+                # if it is accepted, end of the search!
+                all_filters.append(matching_filter(filter, rule))
+                # non-matching values can keep evaluating rules. recurse.
+                search(remaining_rules, non_matching_filter(filter, rule))
+            elif rule["destination"] == "R":
+                # ignore the rejected matching values
+                # recurse with the non-matching values:
+                search(remaining_rules, non_matching_filter(filter, rule))
+            elif rule["destination"] in workflows:
+                search(
+                    workflows[rule["destination"]],
+                    matching_filter(filter, rule, workflows),
+                )
+                search(remaining_rules, non_matching_filter(filter, rule, workflows))
+            else:
+                print("unknown destination")
+                exit()
+        else:
+            if rule["destination"] == "A":
+                all_filters.append(filter)
+            elif rule["destination"] in workflows:
+                new_rules = workflows[rule["destination"]]
+                search(new_rules, filter)
+
+    search(workflows["in"], default_filter())
+
+    return all_filters
+
+
 def part_one(data):
     workflows, parts = parsed(data)
     results = run_sorting_process(parts, workflows)
@@ -95,8 +208,16 @@ def part_two(data):
     workflows, _ = parsed(data)
     # TODO: find all "A" paths through workflows
     # for each path, track min/max requirements for the path
-    total = sum([sum(accepted.values()) for accepted in results["A"]])
-    print(total)
+    combined = {
+        "x": [None] * 4000,
+        "m": [None] * 4000,
+        "a": [None] * 4000,
+        "s": [None] * 4000,
+    }
+    for filter in find_all_filters(workflows):
+        # for category in filter:
+        #     print(filter[category]['min'])
+        print(filter)
 
 
 if __name__ == "__main__":
