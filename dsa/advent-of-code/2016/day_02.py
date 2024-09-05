3428f6113434973e770d545aee3baf30dff9d8e6
Mikhail Golubitsky <mike617@gmail.com>
2024-09-05 04:26:55 -0400

    day 2: solve part 2

--- dsa/advent-of-code/2016/day_02.py
+++ dsa/advent-of-code/2016/day_02.py
@@ -1,14 +1,14 @@
-KEYPAD = ["123", "456", "789"]
+KEYPAD = ["XX1XX", "X234X", "56789", "XABCX", "XXDXX"]
 
 
-def part_one(data):
+def solution(data):
     instruction_sets = [list(line.strip()) for line in data]
     vector_by_heading = {"R": (0, 1), "L": (0, -1), "U": (-1, 0), "D": (1, 0)}
     pos = (1, 1)
 
     def in_bounds(pos):
         y, x = pos
-        return y < 3 and x < 3 and x >= 0 and y >= 0
+        return y < 5 and x < 5 and x >= 0 and y >= 0 and KEYPAD[y][x] != "X"
 
     result = []
     for instruction_set in instruction_sets:
@@ -25,4 +25,4 @@ def part_one(data):
 if __name__ == "__main__":
     with open("day_02_input.txt", "r") as file:
         data = file.readlines()
-    part_one(data)
+    solution(data)
