3cd52a063bae87d3c16f34cd79793816a6ecefaf
Mikhail Golubitsky <mike617@gmail.com>
2024-09-06 23:08:47 -0400

    day 7: solve part 2

--- dsa/advent-of-code/2016/day_07.py
+++ dsa/advent-of-code/2016/day_07.py
@@ -14,6 +14,23 @@ def has_abba(string):
     return False
 
 
+def bab_sequences(string):
+    result = []
+
+    if len(string) < 3:
+        return result
+
+    i = 0
+
+    while i < len(string) - 2:
+        s = string[i : i + 3]
+        if s[0] != s[1] and s[0] == s[2]:
+            result.append(s)
+        i += 1
+
+    return result
+
+
 def parsed(line):
     standard = []
     hypernet = []
@@ -49,11 +66,33 @@ def supports_tls(line):
     return False
 
 
+def flatten(matrix):
+    return [item for row in matrix for item in row]
+
+
+def supports_ssl(line):
+    p = parsed(line)
+
+    standard_bab = flatten([bab_sequences(s) for s in p["standard"]])
+    hypernet_bab = set(flatten([bab_sequences(s) for s in p["hypernet"]]))
+
+    for bab in standard_bab:
+        for hyp_bab in hypernet_bab:
+            if bab[0] == hyp_bab[1] and bab[1] == hyp_bab[0]:
+                return True
+
+    return False
+
+
 def part_one(data):
     print(sum([supports_tls(line) for line in data]))
+
+
+def part_two(data):
+    print(sum([supports_ssl(line) for line in data]))
 
 
 if __name__ == "__main__":
     with open("day_07_input.txt", "r") as file:
         data = file.readlines()
-    part_one(data)
+    part_two(data)
