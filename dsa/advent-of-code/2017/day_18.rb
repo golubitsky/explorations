995583e11944c4ccfe06c16bafeaae9ccd0b3533
Mikhail Golubitsky <mike617@gmail.com>
2024-09-15 14:57:47 -0400

    solve day 18 part 2

--- dsa/advent-of-code/2017/day_18.rb
+++ dsa/advent-of-code/2017/day_18.rb
@@ -1,9 +1,9 @@
-PLAY_FREQUENCY = 'snd'
+SEND = 'snd'
 SET = 'set'
 ADD = 'add'
 MULTIPLY = 'mul'
 REMAINDER = 'mod'
-RECOVER_FREQUENCY = 'rcv'
+RECEIVE = 'rcv'
 JUMP = 'jgz'
 
 def integer?(str)
@@ -24,7 +24,6 @@ def parsed_value(raw_value, registers)
 end
 
 def part_one(data)
-  skip_count = 0
   registers = Hash.new(0)
   i = 0
   sound = nil
@@ -34,7 +33,7 @@ def part_one(data)
     value = parsed_value(raw_value, registers)
 
     case instruction
-    when PLAY_FREQUENCY
+    when SEND
       sound = registers[register]
     when SET
       registers[register] = value
@@ -44,7 +43,7 @@ def part_one(data)
       registers[register] *= value
     when REMAINDER
       registers[register] %= value
-    when RECOVER_FREQUENCY
+    when RECEIVE
       return sound unless registers[register].zero?
     when JUMP
       check = integer?(register) ? register.to_i : registers[register]
@@ -55,10 +54,85 @@ def part_one(data)
     end
     i += 1
   end
-  p registers
+end
+
+class Program
+  attr_reader :data, :registers, :q, :send_count
+
+  def initialize(data, id)
+    @index = 0
+    @data = data
+    @registers = Hash.new(0)
+    registers['p'] = id
+    @q = Queue.new
+    @send_count = 0
+  end
+
+  def check_if_done
+    :done if @index == data.length
+  end
+
+  def process_next_instruction
+    return check_if_done if check_if_done
+
+    # puts "program #{registers['p']}: #{@index + 1}"
+    instruction, register, raw_value = data[@index].split
+    value = parsed_value(raw_value, registers)
+
+    case instruction
+    when SEND
+      send_value = integer?(register) ? register.to_i : registers[register]
+      @index += 1
+      @send_count += 1
+      return send_value
+    when SET
+      registers[register] = value
+    when ADD
+      registers[register] += value
+    when MULTIPLY
+      registers[register] *= value
+    when REMAINDER
+      registers[register] %= value
+    when RECEIVE
+      return :waiting if q.empty?
+
+      registers[register] = q.pop
+    when JUMP
+      check = integer?(register) ? register.to_i : registers[register]
+      if check.positive?
+        @index += value
+        return
+      end
+    end
+    @index += 1
+
+    check_if_done
+  end
+
+  def enqueue(value)
+    @q << value
+  end
+end
+
+def part_two(data) # AKA operating system?
+  zero = Program.new(data, 0)
+  one = Program.new(data, 1)
+
+  loop do
+    result_zero = zero.process_next_instruction
+    result_one = one.process_next_instruction
+
+    zero.enqueue(result_one) if result_one.is_a?(Integer)
+    one.enqueue(result_zero) if result_zero.is_a?(Integer)
+
+    break if zero.q.empty? && one.q.empty? && [result_zero, result_one].all? { |r| %i[done waiting].include?(r) }
+  end
+
+  one.send_count
 end
 
 if __FILE__ == $0
   data = File.readlines('day_18_input.txt')
   p part_one(data)
+  p part_two(data)
 end
